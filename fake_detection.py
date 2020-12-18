7bf6873235c235b9d7812c90141a580da65b3269
Divine Mbunga <divinembunga7@gmail.com>
2020-12-18 22:56:16 +0000

    KNN and cross val done (may need remove KFold?)

--- fake_detection.py
+++ fake_detection.py
@@ -1,12 +1,12 @@
 import matplotlib.pyplot as plt
 from sklearn import metrics
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix,mean_squared_error
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split,KFold
 import numpy as np
 from sklearn.pipeline import Pipeline
 import pandas as pd
@@ -15,6 +15,7 @@ import re
 import string
 import seaborn as sns
 
+#nltk.download('stopwords')
 stopwords = nltk.corpus.stopwords.words('english')
 
 
@@ -64,14 +65,42 @@ def logistic_Regression(X_train, X_test, Y_train, Y_test):
     print(lr_pred)
 
 
-def knn_classifier(X_train, X_test, Y_train, Y_test):
+def knn_classifier(x,y,z):
+    kf = KFold(n_splits=5)
+    mean_error = []
+    std_error = []
 
-    pipe1 = Pipeline([('vect', CountVectorizer()), ('tfidf',
+    k_range = [1,3,5,7,10]
+
+    for k in k_range:
+        pipe1 = Pipeline([('vect', CountVectorizer()), ('tfidf',
                                                     TfidfTransformer()), ('model', KNeighborsClassifier(n_neighbors=3, weights='uniform'))])
+        tmp = []
+        plotted = False
+        for train,test in kf.split(x):
+            model_knn = pipe1.fit(x[train], y[train])
+            knn_pred = model_knn.predict(x[test])
+            tmp.append(mean_squared_error(y[test],knn_pred))
 
-    model_knn = pipe1.fit(X_train, Y_train)
-    knn_pred = model_knn.predict(X_test)
-    print(knn_pred)
+            if (not plotted):
+                data ={'resource':z[test],
+                       'label':knn_pred}
+                df = pd.DataFrame(data)
+                sns.countplot(x='label', hue='resource', data= df,palette='deep')
+                #sns.countplot(knn_pred)
+                plt.title(f"KNN Classifier with k ={k}")
+                plt.show()
+                plotted = True
+        
+        mean_error.append(np.array(tmp).mean())
+        std_error.append(np.array(tmp).std())
+
+    plt.errorbar(k_range,mean_error,yerr=std_error)
+    plt.xlabel('k value')
+    plt.ylabel('Mean Square Error')
+    plt.title("k value for KNN performance")
+    plt.show()
+
 
 
 def main():
@@ -86,12 +115,13 @@ def main():
     Y = dataset.iloc[:, 2]
     # sns.countplot(dataset['Label'])
     # plt.show()
-    X_train, X_test, Y_train, Y_test = train_test_split(
-        X, Y, test_size=0.2, random_state=1)
+    X_train, X_test, Y_train, Y_test,Z_train, Z_test = train_test_split(
+        X, Y,Z, test_size=0.2, random_state=1)
 
-    logistic_Regression(X_train, X_test, Y_train, Y_test)
+    #logistic_Regression(X_train, X_test, Y_train, Y_test)
 
-    knn_classifier(X_train, X_test, Y_train, Y_test)
+    #knn_classifier(X_train, X_test, Y_train, Y_test,Z_train, Z_test)
+    knn_classifier(X,Y,Z)
 
 
 if __name__ == '__main__':
